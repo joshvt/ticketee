7d68efdff99b5e46c2b8e976ad833dfd5d3c1e83
jvt <joshvt@gmail.com>
2012-09-18 20:52:38 -0700

    admin only auth for projects

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,9 +10,12 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   config.include EmailSpec::Helpers
+
   config.before do
     ActionMailer::Base.deliveries.clear
   end
+
+  config.include Devise::TestHelpers, :type => :controller
 
   # ## Mock Framework
   #
