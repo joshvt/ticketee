024c95f85ec5b1be39914b9273d252fc2fc189b8
jvt <joshvt@gmail.com>
2012-10-06 22:19:13 -0700

    add default state

---
+++ app/models/add_default_to_states.rb
@@ -0,0 +1,3 @@
+class AddDefaultToStates < ActiveRecord::Base
+  attr_accessible :default
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,15 @@ admin_user.admin = true
 admin_user.confirm!
 Project.create(:name => "Ticketee Beta")
 
-State.create(:name       => "New",
+State.create({:name       => "New",
              :background => "#85FF00",
-             :color      => "white")
+             :color      => "white",
+             :default    => true},
+             :without_protection => true)
 State.create(:name       => "Open",
              :background => "#00CFFD",
              :color      => "white")
 State.create(:name       => "Closed",
              :background => "black",
              :color      => "white")
+
