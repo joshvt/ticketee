451042b6e05715589040850ff0bd875bddd776a9
jvt <joshvt@gmail.com>
2012-10-08 17:57:28 -0700

    ability to add tags to ticket

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,11 @@
 class Comment < ActiveRecord::Base
   before_create :set_previous_state
+  before_create :associate_tags_with_ticket
   after_create :set_ticket_state
 
 
-  attr_accessible :text, :state_id
+  attr_accessible :text, :state_id, :tag_names
+  attr_accessor :tag_names
   validates :text, :presence => true
   belongs_to :user
   belongs_to :state
@@ -22,5 +24,15 @@ class Comment < ActiveRecord::Base
 
   def set_previous_state
     self.previous_state=ticket.state
+  end
+
+  def associate_tags_with_ticket
+    if tag_names
+      tags = tag_names.split(" ").map do |name|
+        Tag.find_or_create_by_name(name)
+      end
+      self.ticket.tags += tags
+      self.ticket.save
+    end
   end
 end
